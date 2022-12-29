21765802b4471675ea210ce4fd8055f44d47ebb6
Siarhei Chekan <unclesamsch@gmail.com>
2022-12-29 16:48:55 +0300

    routing-added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, tap } from 'rxjs';
-import { IProduct } from './models/product';
-import { ModalService } from './services/modal.service';
-import { ProductService } from './services/products.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'angular-app';
-  // products: IProduct[] = [];
-  loading = false;
-  queryString = '';
 
-  // products$: Observable<IProduct[]>;
-
-  constructor(
-    public productService: ProductService,
-    public modalService: ModalService
-  ) {
-  }
-
-  ngOnInit(): void {
-    this.loading = true;
-    this.productService.getAll().subscribe(() => {
-      // this.products = products;
-      this.loading = false;
-    });
-    // this.products$ = this.productService.getAll().pipe(tap(() => this.loading = false));
-  }
-}
+export class AppComponent {}
